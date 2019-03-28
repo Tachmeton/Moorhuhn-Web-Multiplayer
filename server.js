14b3dc2014219aa1b3def476e17c6fd42e3c144d
Franz Bubel <Franz.Bubel@ts.fujitsu.com>
2019-03-28 12:00:52 +0100

    changed file structure, added node demo

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000);
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/wasIstBasti", function(req, res) {
+    res.send("ein kek");
+});
+
+app.post("/asdasd", function(req, res) {
+
+});
+
+app.delete("/asldew", function() {
+
+});
+
+function getGames() {
+    // Datenbankverbindung aufbauen
+
+}
