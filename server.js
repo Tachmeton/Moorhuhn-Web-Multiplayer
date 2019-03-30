d57eda8d24f248dcdabe0c0566ab5d1a6d43c3d9
Franz <inf17052@lehre.dhbw-stuttgart.de>
2019-03-30 02:24:57 +0100

    added socket.io interfaces to games.js

--- server.js
+++ server.js
@@ -8,17 +8,4 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/wasIstBasti", function(req, res) {
-    res.send("ein kek");
-});
-
-app.post("/asdasd", function(req, res) {
-
-});
-
-app.delete("/asldew", function() {
-
-});
-
-
 app.listen(3000);
--- static/game.js
+++ static/game.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
 
     $("#start").click(startGame);
 
-    const chicks = [
+    const c = document.getElementById("game");
+
+    // initialize necessary global variables
+    let chicks = [
         {
             x: 140,
             y: 20,
@@ -22,17 +25,84 @@ $(document).ready(function() {
             direction: 'w'
         }
     ];
-
+    let myChick = 1;
     const xSpeed = 5;
     const ySpeed = 2;
-
     let pic = new Image();
     let picReverse = new Image();
     pic.src = "img/copyrightChick.png";
     picReverse.src = "img/copyrightChickReverse.png";
 
+    // make socket connection
+    const socket = io('http://localhost');
+
+    socket.on('connect', function() {
+        console.log("socket connection established");
+    });
+
+    socket.on('connect_error', function(message) {
+        console.log("error @ establishing socket connection: " + message);
+    });
+
+    socket.on('syncChicks', function(syncedChicks) {
+        chicks = syncedChicks;
+    });
+
+    socket.on('updateChick', function(chicken) {
+        chicks[chicken.id] = chicken;
+    });
+
+    socket.on('disconnect', function() {
+        console.log("socket connection was closed");
+    })
+
+    // register keypresses
+    document.onkeydown = sendChickControl;
+
+    // register mouse click
+    document.getElementById("game").onclick = sendHunterShot;
+
+    function sendChickControl(e) {
+        let direction;
+        e = e || window.event;
+        if (e.keyCode == '38') { // up key
+            direction = 'n';
+        }
+        else if (e.keyCode == '40') { // down key
+            direction = 's';
+        }
+        else if (e.keyCode == '37') { // left key
+            direction = 'w';
+        }
+        else if (e.keyCode == '39') { // right key
+            direction = 'e';
+        }
+
+        // only emit to server if direction changed
+        if(chicks[myChick].direction != direction) {
+            io.emit('chickInput', {
+                id:chicks[myChick].id,
+                x:chicks[myChick].x,
+                y:chicks[myChick].y,
+                direction:direction
+            });
+        }
+    }
+
+    function sendHunterShot(e) {
+        e = e || window.event;
+
+        var rect = c.getBoundingClientRect();
+        var canvasPosX = event.clientX - rect.left;
+        var canvasPosY = event.clientY - rect.top;
+
+        io.emit('hunterShot', {
+            x: canvasPosX,
+            y: canvasPosY
+        });
+    }
+
     function startGame() {
-        const c = document.getElementById("game");
         const ctx = c.getContext("2d");
 
         const xMax = c.width;
@@ -47,30 +117,13 @@ $(document).ready(function() {
 
     function gameLoop(ctx, xMax, yMax) {
         ctx.clearRect(0,0, xMax, yMax);
-        updateDirections(chicks);
+        updateDirections(chicks); // uncomment when server is working
         updateChicks(chicks, xMax, yMax);
         drawChicks(ctx, chicks);
-
-
-        /*x += xSpeed;
-        y += ySpeed;
-
-        x = (x < 0) ? 0: x;
-        x = (x > xMax) ? xMax: x;
-
-        y = (y < 0) ? 0: y;
-        y = (y > yMax) ? yMax: y;
-
-        if(xSpeed >= 0) {
-            ctx.drawImage(picReverse, x, y);
-        } else {
-            ctx.drawImage(pic, x, y);
-        }*/
     }
 
     function updateDirections(chicks) {
-
-        for(let i = 0; i < chicks.length; i++) {
+        for(let i = 1; i < chicks.length; i++) {
             const random = Math.random();
             if(Math.round(random * 100) % 15 == 1) {
                 switch (true) {
@@ -118,7 +171,7 @@ $(document).ready(function() {
 
     function drawChicks(ctx, chicks){
         for(let i = 0; i < chicks.length; i++) {
-            if(chicks[i].xSpeed >= 0) {
+            if(chicks[i].direction === 'e') {
                 ctx.drawImage(picReverse, chicks[i].x, chicks[i].y);
             } else {
                 ctx.drawImage(pic, chicks[i].x, chicks[i].y);
